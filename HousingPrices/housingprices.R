54ad716c95eacdeb506a91750661b9c31e0f70dd
robbyjeffries1 <robby.jeffries@att.net>
2021-08-28 00:31:52 -0500

    housing prices code

--- HousingPrices/housingprices.R
+++ HousingPrices/housingprices.R
@@ -3,3 +3,79 @@
 ### 8-27-2021
 ### Reference: https://www.kaggle.com/rtatman/welcome-to-data-science-in-r
 
+# load in packages we'll use
+library(tidyverse) # utility functions
+library(rpart) # for regression trees
+library(randomForest) # for random forests
+
+# read the data and store data in DataFrame titled melbourne_data
+melbourne_data <- read_csv('/Users/robbyjeffries/PersonalProjects/HousingPrices/melb_data.csv')
+
+# print a summary of the data in Melbourne data
+summary(melbourne_data)
+
+# train a decision tree based on our dataset
+fit <- rpart(Price ~ Rooms + Bathroom + Landsize + BuildingArea + YearBuilt  + Lattitude + Longtitude, data = melbourne_data)
+
+# plot our regression tree
+plot(fit, uniform=TRUE)
+# add text labels & make them 60% as big as they are by defauld
+text(fit, cex=0.6)
+
+print("Making predicitons for the following 5 houses:")
+print(head(melbourne_data))
+
+print("The predictions are")
+print(predict(fit, head(melbourne_data)))
+
+print("Actual price")
+print(head(melbourne_data$Price))
+
+## Determining how accurate the model is
+
+# package with the mae function
+library(modelr)
+
+# get the mean average error for our model
+mae(model = fit, data = melbourne_data)
+
+# split our data so that 30% is in the test set and 70% is in the training set
+splitData <- resample_partition(melbourne_data, c(test = 0.3, train = 0.7))
+
+# how many cases are in test & training set?
+lapply(splitData, dim)
+
+# fit a new model to our training set
+fit2 <- rpart(Price ~ Rooms + Bathroom + Landsize + BuildingArea + YearBuilt + Lattitude + Longtitude, data = splitData$train)
+
+# get the mean average error for our new model, based on our test data
+mae(model = fit2, data = splitData$test)
+
+# a function to get the maximum average error for a given max depth. You should pass in
+# the target as the name of the target column and the predictors as vector where
+# each item in the vector is the name of the column
+get_mae <- function(maxdepth, target, predictors, training_data, testing_data){
+  
+  # turn the predictors & target into a formula to pass to rpart()
+  predictors <- paste(predictors, collapse="+")
+  formula <- as.formula(paste(target,"~",predictors,sep = ""))
+  
+  # build our model
+  model <- rpart(formula, data = training_data, 
+                 control = rpart.control(maxdepth = maxdepth))
+  
+  # get the mae
+  mae <- mae(model, testing_data)
+  return(mae)
+}
+
+# target & predictors to feed into our formula
+target <- "Price"
+predictors <- c("Rooms","Bathroom","Landsize","BuildingArea","YearBuilt","Lattitude","Longtitude")
+
+# get the MAE for maxdepths between 1 & 10
+for(i in 1:10){
+  mae <- get_mae(maxdepth = i, target = target, predictors = predictors,
+                 training_data = splitData$train, testing_data = splitData$test)
+  print(glue::glue("Maxdepth: ",i,"\t MAE: ",mae))
+}
