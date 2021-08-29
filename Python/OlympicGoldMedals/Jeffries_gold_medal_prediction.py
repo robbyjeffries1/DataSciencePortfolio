91bf70b438143ad78afcbc5f249494e39aaff20a
robbyjeffries1 <robby.jeffries@att.net>
2021-08-29 17:34:40 -0500

    adding test

--- Python/OlympicGoldMedals/Jeffries_gold_medal_prediction.py
+++ Python/OlympicGoldMedals/Jeffries_gold_medal_prediction.py
@@ -12,7 +12,7 @@ The gold medal count per year will be imported from a .csv file, and
 statistical methods will be used to predict the medal count for the
 2020 games.
 """
-
+# testing
 import numpy as np
 import pandas as pd 
 from sklearn.linear_model import LinearRegression
