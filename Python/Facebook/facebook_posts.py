61a6aa0db943eda87a532da094782bbff7086f26
robbyjeffries1 <robby.jeffries@att.net>
2021-08-29 19:09:07 -0500

    add references

--- Python/Facebook/facebook_posts.py
+++ Python/Facebook/facebook_posts.py
@@ -5,6 +5,8 @@ Created on Sun Aug 29 17:38:39 2021
 
 @author: robbyjeffries
 
+Reference: https://www.dataquest.io/blog/analyze-facebook-data-python/
+
 This file creates a graph, showing my frequency of Facebook posts per month.
 """
 
